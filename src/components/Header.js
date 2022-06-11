b6a590e64f91db26620b829fab2913cce9717f4c
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2022-06-11 17:10:24 -0500

    Cambiando leyenda de encabezado

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ function Header() {
     return (
         <>
             <h1 className="display-4">Maps App</h1>
-            <p className="lead">App desarrollada utilizando Google Maps Javascript API para el cálculo de distancias.</p>
+            <p className="lead">
+                Lost? Don't worry, we'll take you wherever you need!.</p>
             <hr/>
         </>
     )
